33fa72c066e70a236af1ad9c55d916fe14dd461b
kirifyang <167179109+kirifyang@users.noreply.github.com>
2024-06-26 09:18:48 +0000

    Add script file.

---
+++ setting.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install ClickHouse
+if [ ! -f click_data ]; then
+    mkdir click_data
+fi
+
+cd click_data
+
+if [ ! -f clickhouse ]; then
+    curl https://clickhouse.com/ | sh
+fi
+
+export CLICKHOUSE_CONFIG_FILE=clickhouse_config.xml
+echo $CLICKHOUSE_CONFIG_FILE
+
+killall clickhouse
+
+./clickhouse server --config-file="../$CLICKHOUSE_CONFIG_FILE"
+
+# Wait for server deployment
+sleep 10
+
+# Execute the command after server deployment
+./clickhouse client
