64f9fe108a34b19d0da142509e8a5fcafd9bd654
kirifyang <167179109+kirifyang@users.noreply.github.com>
2024-06-26 09:19:09 +0000

    Update the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-  println!("Hello, world!");
+use chrono::Utc;
+use futures_util::stream::StreamExt;
+use klickhouse::*;
+
+#[derive(Row, Debug, Default)]
+pub struct UserData {
+    id: Uuid,
+    username: String,
+    created_at: DateTime,
+}
+
+#[tokio::main]
+async fn main() {
+    // env_logger::Builder()::new()
+    //   .parse_env(env_logger::Env::default().default_filter_or("info")).init();
 }
