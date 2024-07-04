82e0d90000054035beb1a4f0b3f82c7793560982
kirifyang <167179109+kirifyang@users.noreply.github.com>
2024-07-04 09:58:33 +0000

    Feat: Use semaphore to limit the num of async tasks.

--- src/db/ops.rs
+++ src/db/ops.rs
@@ -4,8 +4,14 @@ use flate2::read::GzDecoder;
 use klickhouse::*;
 use std::fs::File;
 use std::io::Read;
+use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
+use tokio::sync::Semaphore;
+use tokio::time::{sleep, Duration};
 
 const CLICKHOUSE_USER_FILE_PATH: &str = "./clickhouse/click_data/user_files";
+// semaphores are used to limit the number of concurrent tasks
+const TASK_SEMAPHORE_LIMIT: usize = 3;
+const TASK_WAIT_TIME: u64 = 5;
 
 #[allow(dead_code)]
 pub async fn insert_table(client: &Client, table_name: &str, row: Vec<Opensky>) {
@@ -29,14 +35,20 @@ pub async fn create_table(client: &Client, table_name: &str, schema: &str) {
 }
 
 pub async fn insert_table_from_files(client: &Client, table_name: &str) {
-    let files = get_csvs_names_grouped_by_date(); // この関数も非同期にする必要があるかもしれません
+    let files = get_csvs_names_grouped_by_date(); 
 
+    let semaphore = Arc::new(Semaphore::new(TASK_SEMAPHORE_LIMIT));
+    let completed_tasks = Arc::new(AtomicUsize::new(0));
     let mut tasks = vec![];
 
     for file in files {
         let client = client.clone();
         let table_name = table_name.to_string();
+        let semaphore_clone = semaphore.clone();
+        let completed_clone = completed_tasks.clone();
         let task = tokio::spawn(async move {
+            let _permit = semaphore_clone.acquire().await.expect("Failed to acquire semaphore permit");
+            sleep(Duration::from_secs(TASK_WAIT_TIME)).await;
             println!("Processing file: {}", file);
             let file_path = format!("{}/{}", CLICKHOUSE_USER_FILE_PATH, file);
             let file = File::open(&file_path).expect("Failed to open file");
@@ -50,10 +62,13 @@ pub async fn insert_table_from_files(client: &Client, table_name: &str) {
                 "INSERT INTO {} FORMAT CSVWithNames\n{}",
                 table_name, output_str
             );
-
             client.execute(&query).await.unwrap();
+            completed_clone.fetch_add(1, Ordering::SeqCst);
         });
         tasks.push(task);
+        if completed_tasks.load(Ordering::SeqCst) % TASK_SEMAPHORE_LIMIT == 0 {
+            println!("Completed tasks: {}", completed_tasks.load(Ordering::SeqCst));
+        }
     }
 
     for task in tasks {
