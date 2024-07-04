d7c69320ce729ea26ccecf49156c5f2b9a918244
kirifyang <167179109+kirifyang@users.noreply.github.com>
2024-07-04 15:18:52 +0900

    Use async to improve performace

--- src/db/ops.rs
+++ src/db/ops.rs
@@ -1,7 +1,11 @@
+use crate::db::file::get_csvs_names_grouped_by_date;
 use crate::db::schema::Opensky;
+use flate2::read::GzDecoder;
 use klickhouse::*;
-use std::fs;
-use std::process::Command;
+use std::fs::File;
+use std::io::Read;
+use std::sync::Arc;
+use tokio::{task, runtime::Runtime};
 
 const CLICKHOUSE_USER_FILE_PATH: &str = "./clickhouse/click_data/user_files";
 
@@ -25,35 +29,34 @@ pub async fn create_table(client: &Client, table_name: &str, schema: &str) {
     client.execute(query).await.unwrap();
 }
 
-pub async fn insert_table_from_files(client: &Client, table_name: &str) {
-    let files = fs::read_dir(CLICKHOUSE_USER_FILE_PATH)
-        .unwrap()
-        .filter_map(Result::ok)
-        .filter(|entry| {
-            let binding = entry.file_name();
-            let file_name_str = binding.to_string_lossy();
-            let file_name = file_name_str.as_ref();
-            // file_name.starts_with("flightlist_") && file_name.ends_with(".csv.gz")
-            // for testing purposes, only use the 20190131 file
-            file_name.starts_with("flightlist_") && file_name.ends_with("20190131.csv.gz")
-        })
-        .map(|entry| entry.path())
-        .collect::<Vec<_>>();
+// need to improve this
+pub async fn insert_table_from_files(client:&Client, table_name:&str ) {
+    let files = get_csvs_names_grouped_by_date(); // この関数も非同期にする必要があるかもしれません
+
+    let mut tasks = vec![];
 
     for file in files {
-        let output = Command::new("gzip")
-            .arg("-c")
-            .arg("-d")
-            .arg(file.to_str().unwrap())
-            .output()
-            .unwrap();
+        let client = client.clone();
+        let table_name = table_name.to_string();
+        let task = tokio::spawn(async move {
+            println!("Processing file: {}", file);
+            let file_path = format!("{}/{}", CLICKHOUSE_USER_FILE_PATH, file);
+            let mut file = File::open(&file_path).expect("Failed to open file");
+            let mut decoder = GzDecoder::new(file);
+            let mut output_str = String::new();
+            decoder.read_to_string(&mut output_str).expect("Failed to read gzip file");
 
-        let output_str = String::from_utf8_lossy(&output.stdout);
-        let query = format!(
-            "INSERT INTO {} FORMAT CSVWithNames\n{}",
-            table_name, output_str
-        );
+            let query = format!(
+                "INSERT INTO {} FORMAT CSVWithNames\n{}",
+                table_name, output_str
+            );
 
-        client.execute(&query).await.unwrap();
+            client.execute(&query).await.unwrap();
+        });
+        tasks.push(task);
+    }
+
+    for task in tasks {
+        task.await.unwrap();
     }
 }
